45debc58dc35e1b2400818c229f79e323371aec8
Jennings Anderson <jenningsa@meta.com>
2023-04-28 15:40:00 -0700

    auto-docs

---
+++ gen_doc.sh
@@ -0,0 +1,11 @@
+#
+# https://github.com/coveooss/json-schema-for-humans
+#
+# Generates human-readable HTML in the `docs` folder from the
+# contents of the `schema folder`
+#
+# pip install json-schema-for-humans
+#
+# and then run this file.
+
+generate-schema-doc --config-file=documentation_config.yaml schema docs/auto_gen
