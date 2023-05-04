132af99a395736cf061dfc50d555608f13ede9a9
Jennings Anderson <jenningsa@meta.com>
2023-05-04 09:00:19 -0700

    relabeling the sidebar for simplicity

--- documentation_website/docs/index.md
+++
@@ -1,8 +0,0 @@
----
-position: 0
-id: Documentation
-slug: /
----
-
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus blandit orci, quis tristique nisi viverra nec. Pellentesque ullamcorper vitae mi ac tincidunt. Nullam eu felis sed magna ultricies auctor. Duis cursus ante ut condimentum iaculis. Sed congue ultricies erat non accumsan. Phasellus lobortis, lacus ut sodales porta, ante turpis tempus dui, et euismod urna urna eu augue. Cras eu vulputate neque. Ut dui lorem, sodales vel nunc at, mollis mollis dui. Ut porta eros sodales, aliquam risus sit amet, vehicula quam. Proin accumsan mollis mauris, vel hendrerit augue molestie sed.
--- documentation_website/docusaurus.config.js
+++ documentation_website/docusaurus.config.js
@@ -43,9 +43,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/'
         },
-        blog: {
-          showReadingTime: true,
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -71,57 +69,10 @@ const config = {
             position: 'left',
             label: 'Documentation',
           },
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   href: 'https://github.com/facebook/docusaurus',
-          //   label: 'GitHub',
-          //   position: 'right',
-          // },
         ],
       },
       footer: {
         style: 'dark',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'More',
-          //   items: [
-          //     {
-          //       label: 'Blog',
-          //       to: '/blog',
-          //     },
-          //     {
-          //       label: 'GitHub',
-          //       href: 'https://github.com/facebook/docusaurus',
-          //     },
-          //   ],
-          // },
-        // ],
         copyright: `Copyright © ${new Date().getFullYear()} Overture Maps Foundation. Built with Docusaurus.`,
       },
       prism: {
