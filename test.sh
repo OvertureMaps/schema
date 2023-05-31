122744f48251504289a2555368edec9ed097321b
schapper <schapper@amazon.com>
2023-05-31 15:08:20 -0700

    Support expected error assertions within the counterexamples
    This change enables us to assert that a particular counterexample
    results in a particular validation error, thus preventing the build-up
    of cruft from pointless counterexamples which aren't testing anything
    of value.
    
    As part of this change, I had to delete the `time-no-timezone.yaml`
    example because it was an invalid example that had been silently
    missed previously but started failing when I added the `-assertformat`
    assertion requirement to the `jv` command line.

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ function verify() {
   local mode="$1"
   local instance_file="${2:-}"
 
-  local -a jv_args=("$schema_file")
+  local -a jv_args=(-assertformat -assertcontent "$schema_file")
   if [ -n "$instance_file" ]; then
     jv_args+=("$instance_file")
   fi
@@ -18,10 +18,23 @@ function verify() {
   quiet)
     jv "${jv_args[@]}" 2>/dev/null
     ;;
+  simple)
+    jv "${jv_args[@]}"
+    ;;
   *)
-    jv --output "$mode" "${jv_args[@]}"
+    jv -output "$mode" "${jv_args[@]}"
     ;;
   esac
+}
+
+function expected_errors() {
+  local instance_file="$1"
+  local type
+  type=$(yq -r '.properties | type' "$instance_file")
+  if [ "$type" != "object" ]; then
+    return 1
+  fi
+  yq -r '(.properties.extExpectedErrors // []) | .[]' "$instance_file"
 }
 
 echo "---- VERIFYING schema ----"
@@ -50,21 +63,38 @@ done
 
 echo
 
-# TODO: A major shortcoming of the counterexamples is that there is no
-#       way to specify HOW they fail. Consequently if a counterexample
-#       becomes totally obsolete because we ripped out that part of the
-#       schema, we don't get any notice that the counterexample is now
-#       irrelevant. Pointless cruft will build up over time until this
-#       is fixed.
-
 echo "---- VERIFYING counterexamples ----"
+yq_installed=
+if command -v yq >/dev/null; then
+  yq_installed=true
+else
+  >&2 printf "WARNING: yq is not installed. Install yq for higher-fidelity counterexample testing."
+fi
 find counterexamples -type f | sort | while read -r instance_file; do
   printf "%s..." "$instance_file"
-  if ! verify quiet "$instance_file"; then
-    echo OK
-  else
+  declare -a expected_errors
+  if verify quiet "$instance_file"; then
     echo FAILED
     printf "\ncounterexample instance '%s' is EXPECTED to fail validation but ACTUALLY it passed.\n" "$instance_file"
     exit 1
+  elif [ -z "$yq_installed" ] || ! expected_errors "$instance_file" | mapfile -t expected_errors || [ ${#expected_errors} == 0 ]; then
+    echo OK
+  else
+    declare -a actual_errors
+    (verify simple "$instance_file" || true) 2>&1 | mapfile -t actual_errors
+    for expected_error in "${expected_errors[@]}"; do
+      for actual_error in "${actual_errors[@]}"; do
+        if [[ "$actual_error" == *"$expected_error"* ]]; then
+          continue 2
+        fi
+      done
+      echo FAILED
+      printf "\ncounterexample instance '%s' is EXPECTED to trigger the following validation error but ACTUALLY it did not.\n\n" "$instance_file"
+      printf "%s\n" "------------------------ MISSED EXPECTED ERROR -----------------------"
+      printf "%s\n\n" "$expected_error"
+      printf "%s\n" "---------------------- ACTUAL VALIDATION OUTPUT ----------------------"
+      printf "%s\n" "${actual_errors[@]}"
+      exit 1
+    done
   fi
 done
