48e370e824ea9ec0bbee99366efb28b8cc567047
schapper <schapper@amazon.com>
2023-06-02 08:35:03 -0700

    Build skeleton structure for Docusaurus docs (mega-commit)
    This commit is doing a lot of work, sorry!
    
    Some things involved:
    
    1) Renamed the Docusaurus copy of the schema directory/ to docs/_schema/
       so Docusaurus will always ignore it and not crawl it for possible
       Documentation content.
    
    2) Copied the examples/ directory to docs/_examples/ and created a
       prototype system to display validated schema example files as
       examples within the "Feature Types by Theme" documentation.
    
    3) Reorganized the Documentation website around the following structure:
    
        - Concepts
            - Feature
            - Themes
                - ...
        - Feature Types by Theme
            - addresses
                - address
            - admins
                - adminBoundary
                - locality
            - buildings
                - footprint
            - places
                - place
            - transportation
                - connector
                - segment
    
    4) Create a basic structure for the "Feature Type by Theme" pages,
       for example a little mini-table about their geometry types etc.
    
    5) Sketched out some initial text for the "segment" and "connector"
       feature type documentation.
    
    6) Broke the home page for the time being (it's a 404). Until we
       fix it you have to click "Documentation" to get to the nav tree.
    
    7) Removed specific task force documentation files under a directory
       named task-force-docs/ to reduce pollution in the repository root.
    
    Plus some other stuff I'm sure is good but I frankly can't remember.

--- docusaurus/src/YAML_FILE_TREE.js
+++ docusaurus/src/YAML_FILE_TREE.js
@@ -2,21 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const _default = {
 
-    '/addresses/address.yaml': require('!!raw-loader!@site/docs/yaml/addresses/address.yaml'),
+    '/addresses/address.yaml': require('!!raw-loader!@site/docs/_schema/addresses/address.yaml'),
 
-    '/admins/administrativeBoundaries.yaml': require('!!raw-loader!@site/docs/yaml/admins/administrativeBoundary.yaml'),
-    '/admins/defs.yaml': require('!!raw-loader!@site/docs/yaml/admins/defs.yaml'),
-    '/admins/locality.yaml': require('!!raw-loader!@site/docs/yaml/admins/locality.yaml'),
+    '/admins/administrativeBoundaries.yaml': require('!!raw-loader!@site/docs/_schema/admins/administrativeBoundary.yaml'),
+    '/admins/defs.yaml': require('!!raw-loader!@site/docs/_schema/admins/defs.yaml'),
+    '/admins/locality.yaml': require('!!raw-loader!@site/docs/_schema/admins/locality.yaml'),
 
-    '/buildings/footprint.yaml': require('!!raw-loader!@site/docs/yaml/buildings/footprint.yaml'),
+    '/buildings/footprint.yaml': require('!!raw-loader!@site/docs/_schema/buildings/footprint.yaml'),
 
-    '/places/place.yaml': require('!!raw-loader!@site/docs/yaml/places/place.yaml'),
+    '/places/place.yaml': require('!!raw-loader!@site/docs/_schema/places/place.yaml'),
 
-    '/transportation/connector.yaml': require('!!raw-loader!@site/docs/yaml/transportation/connector.yaml'),
-    '/transportation/segment.yaml': require('!!raw-loader!@site/docs/yaml/transportation/segment.yaml'),
+    '/transportation/connector.yaml': require('!!raw-loader!@site/docs/_schema/transportation/connector.yaml'),
+    '/transportation/segment.yaml': require('!!raw-loader!@site/docs/_schema/transportation/segment.yaml'),
 
-    '/defs.yaml': require('!!raw-loader!@site/docs/yaml/defs.yaml'),
-    '/schema.yaml': require('!!raw-loader!@site/docs/yaml/schema.yaml')
+    '/defs.yaml': require('!!raw-loader!@site/docs/_schema/defs.yaml'),
+    '/schema.yaml': require('!!raw-loader!@site/docs/_schema/schema.yaml')
 
 };
 export { _default as default };
