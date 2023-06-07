2e82bd3fc2290a2fe3ed72c0f1f8fa0a9d3cdec6
schapper <schapper@amazon.com>
2023-06-07 15:57:00 -0700

    Consolidate sidebar into sidebars.js, fix 404 on /, fix npm run build

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docs',
             position: 'left',
             label: 'Documentation',
           },
