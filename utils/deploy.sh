839de3e57995db80bb7d5e543302a376934cd713
Jennings Anderson <jennings.anderson@gmail.com>
2023-06-14 15:10:53 -0700

    Deploy script
    Testing (a) deploy script

---
+++ utils/deploy.sh
@@ -0,0 +1,32 @@
+# The deploy script that prepares the schema-wg/main repository to publish at schema/main
+
+# 1. Create a new branch for publishing
+git checkout -b "publication_test"
+
+# 2. Remove extra github workflows
+git rm .github/workflows/github-actions-copy-latest-docs-to-staging.yaml
+git rm .github/workflows/github-actions-copy-pr-docs-to-staging.yaml
+
+# 3. Remove ISSUE_TEMPLATE
+git rm .github/ISSUE_TEMPLATE/*
+
+# 4. Remove other irrelevant files
+git rm submission_process.svg
+
+# 4. Temporary: empty the repository
+git rm .gitignore
+git rm GLOSSARY.md
+git rm LICENSE
+git rm README.md
+git rm SCHEMA_CONVENTIONS.md
+
+git rm -r utils/*
+git rm -r docusaurus/*
+git rm -r examples/*
+git rm -r counterexamples/*
+git rm -r schema/*
+git rm -r task-force-docs/*
+
+git status
+# git commit -am "Ready for publication to schema/main"
+# git git
