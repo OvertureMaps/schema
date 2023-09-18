01a163b72a32435589207ad746da716890af2c07
Jennings Anderson <jennings.anderson@gmail.com>
2023-09-18 14:16:12 -0700

    adding building_part to documentation

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -76,6 +76,7 @@ const sidebars = {
           collapsed: false,
           items: [
             'reference/buildings/building',
+            'reference/buildings/building_part'
           ]
         },
         {
