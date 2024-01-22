bce557aae70a99131096a178f0aefcfd3851e535
Jennings Anderson <jennings.anderson@gmail.com>
2024-01-22 11:25:56 -0800

    upgrading to docusaurus 3.0

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 const defaultUrl = 'https://docs.overturemaps.org';
 const defaultBaseUrl = '/';
@@ -98,6 +99,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['bash', 'diff', 'json'],
       },
     }),
 };
