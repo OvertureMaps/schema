da1466a3a525279acba6e7b3091b6a72318ea555
Victor Schappert <schapper@amazon.com>
2024-02-16 16:52:35 -0800

    Convert property names and enumeration member names to snake_case
    This commit switches properties and enumerators from `camelCase` naming
    to `snake_case` naming.
    
    The purpose is to improve compatibility with SQL based tools. Because
    SQL uses case-insensitive identifiers for historical reasons, SQL-based
    tools cause problems and information loss with camel-casing because they
    tend to treat all identifiers as if they were alllowercase or ALLCAPS.
    
    In the Overture Schema Task Force meeting of 2024-02-07, the task force
    decided to adopt the snake_case proposal:
    
    https://github.com/OvertureMaps/schema-wg/issues/272
    
    The proposal originated in Jake's Schema Friction discussion document
    on the Overture Confluence:
    
    https://wiki.overturemaps.org/x/SQGAAQ
    
    While we believe that snake_case is slightly unconventional, and dare I
    (Vic) say also more unsightly in the JSON context, we feel that for SQL
    compatibility reasons, the pros of changing outweigh the cons.

---
+++ docusaurus/sidebars.js
@@ -0,0 +1,149 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    /*
+    Overview
+    */
+    {
+      type: 'category',
+      label: 'Overview',
+      collapsed: false,
+      items: [
+        {
+          type: 'doc',
+          id: 'overview/index'
+        },
+        {
+          type: 'category',
+          label: 'Feature Model',
+          link: {
+            type: 'doc',
+            id: 'overview/feature-model/index'
+          },
+          collapsed: false,
+          items: [
+            // 'overview/feature-model/geojson',
+            'overview/feature-model/names',
+            'overview/feature-model/scoping-rules',
+          ]
+        }
+
+      ]
+    },
+    /*
+      Themes
+    */
+    {
+      type: 'category',
+      label: 'Schema Themes',
+      link: {
+        type: 'doc',
+        id: 'themes/themes'
+      },
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id: 'themes/admins/admins',
+          label: 'Admins'
+        },
+        'themes/base/index',
+        'themes/buildings/building',
+        'themes/places/place',
+        {
+          type: 'category',
+          label: 'Transportation',
+          link: {
+            type: 'doc',
+            id: 'themes/transportation/index',
+          },
+          collapsed: true,
+          items: [
+            'themes/transportation/shape-connectivity',
+            'themes/transportation/roads',
+            'themes/transportation/travel-modes',
+          ]
+        }
+      ]
+    },
+    /*
+      Schema Reference
+    */
+    {
+      type: 'category',
+      label: 'Schema Reference',
+      link: {
+        type: 'generated-index',
+        slug: '/reference',
+      },
+      collapsed: true,
+      items: [
+        {
+          type: 'category',
+          label: 'admins',
+          collapsed: false,
+          items: [
+              'reference/admins/administrative-boundary',
+              'reference/admins/locality',
+              'reference/admins/locality-area',
+          ]
+        },
+        {
+          type: 'category',
+          label: 'base',
+          collapsed: false,
+          items: [
+              'reference/base/land',
+              'reference/base/land-use',
+              'reference/base/water',
+          ]
+        },
+        {
+          type: 'category',
+          label: 'buildings',
+          collapsed: false,
+          items: [
+            'reference/buildings/building',
+            'reference/buildings/part'
+          ]
+        },
+        {
+          type: 'category',
+          label: 'places',
+          collapsed: false,
+          items: [
+            'reference/places/place',
+          ]
+        },
+        {
+          type: 'category',
+          label: 'transportation',
+          collapsed: false,
+          items: [
+            'reference/transportation/connector',
+            'reference/transportation/segment',
+          ]
+        }
+      ]
+    }
+  ],
+
+  gers: [
+    {
+      type: 'category',
+      label: 'Global Entity Reference System',
+      collapsed: true,
+      items: [
+        {
+          type: 'doc',
+          id:'gers/gers',
+          label: 'Overview'
+        },
+        'gers/scenarios',
+        'gers/terminology'
+      ]
+    },
+  ]
+};
+
+module.exports = sidebars;
