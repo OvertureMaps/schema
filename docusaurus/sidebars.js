4ada508fd7f58fd73f2ee3c8b9ca2f8a07ef43fb
Jennings Anderson <jennings.anderson@gmail.com>
2024-04-03 17:12:36 -0600

    Moving GERS to docs

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -27,7 +27,6 @@ const sidebars = {
             'overview/feature-model/scoping-rules',
           ]
         }
-
       ]
     },
     /*
@@ -127,23 +126,6 @@ const sidebars = {
         }
       ]
     }
-  ],
-
-  gers: [
-    {
-      type: 'category',
-      label: 'Global Entity Reference System',
-      collapsed: true,
-      items: [
-        {
-          type: 'doc',
-          id:'gers/gers',
-          label: 'Overview'
-        },
-        'gers/scenarios',
-        'gers/terminology'
-      ]
-    },
   ]
 };
 
