2e1f3a34c6a89e8e7007f2f6a3c02f2cb845ffe3
Jennings Anderson <jenningsa@meta.com>
2024-04-04 11:23:02 -0600

    Switch schema url to schema.overturemaps.org (#160)
    * Switch schema url to schema.overturemaps.org
    
    * Update docusaurus.config.js
    
    Adding blog and community to point back to Overturemaps.org
    
    (We'll need to rebase `dev` on top of main to get these docusaurus changes in)

---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,109 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const {themes} = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
+
+const defaultUrl = 'https://schema.overturemaps.org';
+const defaultBaseUrl = '/';
+
+function getFromEnvironment(variableName, defaultValue) {
+  const environmentValue = process.env[variableName];
+  return environmentValue ? environmentValue : defaultValue;
+}
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Overture Maps Schema Documentation',
+  tagline: '',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: getFromEnvironment('DOCUSAURUS_URL', defaultUrl),
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: getFromEnvironment('DOCUSAURUS_BASE_URL', defaultBaseUrl),
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'OvertureMaps', // Usually your GitHub org/user name.
+  projectName: 'schema', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  themes: ["docusaurus-json-schema-plugin"],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/'
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/omf_logo_transparent.png',
+      navbar: {
+        title: 'Overture Maps',
+        logo: {
+          alt: 'Overture Maps Foundation Logo',
+          src: 'img/omf_logo_transparent.png',
+          href: 'https://overturemaps.org'
+        },
+        items: [
+          {
+            to: 'https://docs.overturemaps.org/',
+            label: 'Docs',
+            target: ''
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'docs',
+            position: 'left',
+            label: 'Schema Reference',
+          },
+          {
+            to: 'https://overturemaps.org/news-blogs/',
+            label: 'Blog',
+          },
+          {
+            to: 'https://docs.overturemaps.org/community/',
+            label: 'Community',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Overture Maps Foundation. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['bash', 'diff', 'json'],
+      },
+    }),
+};
+
+module.exports = config;
