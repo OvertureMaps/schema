2813cb357dabd6754299899e13d0949bb2992566
Alex Iannicelli <atiannicelli@gmail.com>
2024-07-24 15:57:43 -0400

    Add sql query files for mappings

---
+++ task-force-docs/buildings/facade_material_query.sql
@@ -0,0 +1,87 @@
+CASE
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'brick',
+        'slate',
+        'ladrillos',
+        'masonry',
+        'silicate brick',
+        'bricks',
+        'unburnt brick',
+        'silicate_brick',
+        'brick_block',
+        'muddy_brick'
+    ) THEN 'brick'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+      'cement_block',
+      'block',
+      'cement block',
+      'cement_blocks',
+      'cement blocks'
+    ) THEN 'cement_block'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'clay',
+        'mud',
+        'rammed_earth',
+        'loam',
+        'earth',
+        'grass',
+        'pressed_soil_blocks'
+    ) THEN 'clay'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'concrete',
+        'concrete masonry unit',
+        'cement',
+        'concrete_reinforced',
+        'reinforced_concrete'
+    ) THEN 'concrete'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'glass',
+        'mirror'
+    ) THEN 'glass'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'metal',
+        'steel',
+        'metal_plates',
+        'tin',
+        'iron_sheet',
+        'copper',
+        'metal_sheet',
+        'ironsheets',
+        'panel',
+        'aluminium'
+    ) THEN 'metal'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'plaster',
+        'plastered',
+        'hard'
+    ) THEN 'plaster'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'plastic',
+        'vinyl',
+        'plastic_sheeting',
+        'composite',
+        'vinyl_siding'
+    ) THEN 'plastic'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'stone',
+        'limestone',
+        'sandstone',
+        'granite',
+        'marble',
+        'tiles'
+    ) THEN 'stone'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'timber_framing',
+        'traditional'
+    ) THEN 'timber_framing'
+    WHEN lower(trim(element_at(tags, 'building:facade'))) IN (
+        'wood',
+        'reed',
+        'wattle_and_daub',
+        'timber_planks',
+        'wood/masonry',
+        'wood/bamboo',
+        'bamboo'
+    ) THEN 'wood'
+    ELSE NULL
+END
