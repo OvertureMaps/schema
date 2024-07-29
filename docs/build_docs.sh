7512ca99db9c63ea3cf96a958f0296fa46b20fc3
Jennings Anderson <jennings.anderson@gmail.com>
2024-07-29 10:10:09 -0700

    Remove docusaurus from schema

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -2,18 +2,17 @@ echo "Removing and re-fetching Overture documentation into docusaurus directory"
 rm -rf docusaurus
 git clone --depth=1 https://github.com/OvertureMaps/docs docusaurus
 
-echo "Copying Examples"
-mkdir -p docusaurus/docs/_examples
-cp -R ../examples/* docusaurus/docs/_examples/
+echo "Sym-linking examples docs into docusaurus"
+ln -s $(pwd)/../examples $(pwd)/docusaurus/docs/_examples
 
-echo "Copying Schema YAML"
-mkdir -p docusaurus/docs/_schema
-cp -R ../schema/* docusaurus/docs/_schema/
+echo "Sym-linking schema yaml into docusaurus"
+ln -s $(pwd)/../schema $(pwd)/docusaurus/docs/_schema
+
 
 echo "Sym-linking schema docs into docusaurus"
 ln -s $(pwd)/schema $(pwd)/docusaurus/docs/schema
 
 cd docusaurus
-npm install
+npm install --prefer-dedupe
 
 npm run docusaurus start
--- docusaurus/src/components/yamlLoad.js
+++
@@ -1,9 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-
-yaml = require('js-yaml')
-
-function yamlLoad(string){
-    return yaml.load(string)
-}
-
-exports.default = yamlLoad;
