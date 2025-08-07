ff70a6b182eeafb7494075c71037cb721862fa87
Seth Fitzsimmons <sethfitz@amazon.com>
2025-08-07 10:44:15 -0700

    Replace type aliases with NewType to improve type checks
    This allows string aliases (for example) to be treated differently;
    i.e., a CountryCode and a RegionCode are both strings, but they're
    fundamentally incompatible because they represent different things. mypy
    will now enforce this at type checking time (when used in a Python
    context).

--- packages/overture-schema-transportation-theme/src/overture/schema/transportation/types.py
+++ packages/overture-schema-transportation-theme/src/overture/schema/transportation/types.py
@@ -1,6 +1,6 @@
 """Transportation theme type aliases and annotated types."""
 
-from typing import Annotated
+from typing import Annotated, NewType
 
 from pydantic import Field
 
@@ -20,81 +20,116 @@ from .models import (
     WidthRule,
 )
 
-Destinations = Annotated[
-    list[DestinationRule],
-    Field(
-        description="Describes objects that can be reached by following a transportation segment in the same way those objects are described on signposts or ground writing that a traveller following the segment would observe in the real world. This allows navigation systems to refer to signs and observable writing that a traveller actually sees."
-    ),
-]
+Destinations = NewType(
+    "Destinations",
+    Annotated[
+        list[DestinationRule],
+        Field(
+            description="Describes objects that can be reached by following a transportation segment in the same way those objects are described on signposts or ground writing that a traveller following the segment would observe in the real world. This allows navigation systems to refer to signs and observable writing that a traveller actually sees."
+        ),
+    ],
+)
 
-Routes = Annotated[
-    list[RouteReference], Field(description="Routes this segment belongs to")
-]
+Routes = NewType(
+    "Routes",
+    Annotated[
+        list[RouteReference], Field(description="Routes this segment belongs to")
+    ],
+)
 
-AccessRules = Annotated[
-    list[AccessRestrictionRule],
-    Field(min_length=1, description="Rules governing access to this road segment"),
-    UniqueItemsConstraint(),
-]
+AccessRules = NewType(
+    "AccessRules",
+    Annotated[
+        list[AccessRestrictionRule],
+        Field(min_length=1, description="Rules governing access to this road segment"),
+        UniqueItemsConstraint(),
+    ],
+)
 
 
-SpeedLimits = Annotated[
-    list[SpeedLimitRule],
-    Field(min_length=1, description="Rules governing speed on this road segment"),
-    UniqueItemsConstraint(),
-]
+SpeedLimits = NewType(
+    "SpeedLimits",
+    Annotated[
+        list[SpeedLimitRule],
+        Field(min_length=1, description="Rules governing speed on this road segment"),
+        UniqueItemsConstraint(),
+    ],
+)
 
-ProhibitedTransitions = Annotated[
-    list[ProhibitedTransitionRule],
-    Field(
-        description="Rules preventing transitions from this segment to another segment."
-    ),
-]
+ProhibitedTransitions = NewType(
+    "ProhibitedTransitions",
+    Annotated[
+        list[ProhibitedTransitionRule],
+        Field(
+            description="Rules preventing transitions from this segment to another segment."
+        ),
+    ],
+)
 
-RoadFlags = Annotated[
-    list[RoadFlagRule],
-    Field(
-        min_length=1,
-        description="Set of boolean attributes applicable to roads. May be specified either as a single flag array of flag values, or as an array of flag rules.",
-    ),
-    UniqueItemsConstraint(),
-]
+RoadFlags = NewType(
+    "RoadFlags",
+    Annotated[
+        list[RoadFlagRule],
+        Field(
+            min_length=1,
+            description="Set of boolean attributes applicable to roads. May be specified either as a single flag array of flag values, or as an array of flag rules.",
+        ),
+        UniqueItemsConstraint(),
+    ],
+)
 
-RailFlags = Annotated[
-    list[RailFlagRule],
-    Field(
-        min_length=1,
-        description="Set of boolean attributes applicable to railways. May be specified either as a single flag array of flag values, or as an array of flag rules.",
-    ),
-    UniqueItemsConstraint(),
-]
+RailFlags = NewType(
+    "RailFlags",
+    Annotated[
+        list[RailFlagRule],
+        Field(
+            min_length=1,
+            description="Set of boolean attributes applicable to railways. May be specified either as a single flag array of flag values, or as an array of flag rules.",
+        ),
+        UniqueItemsConstraint(),
+    ],
+)
 
-LevelRules = Annotated[
-    list[LevelRule],
-    Field(description="Defines the Z-order, i.e. stacking order, of the road segment."),
-]
+LevelRules = NewType(
+    "LevelRules",
+    Annotated[
+        list[LevelRule],
+        Field(
+            description="Defines the Z-order, i.e. stacking order, of the road segment."
+        ),
+    ],
+)
 
-SubclassRules = Annotated[
-    list[SubclassRule], Field(description="Set of subclasses scoped along segment")
-]
+SubclassRules = NewType(
+    "SubclassRules",
+    Annotated[
+        list[SubclassRule], Field(description="Set of subclasses scoped along segment")
+    ],
+)
 
 # We should likely restrict the available surface types to the subset of the common OSM surface=* tag values that are useful both for routing and for map tile rendering.
-Surfaces = Annotated[
-    list[SurfaceRule],
-    Field(
-        min_length=1,
-        description="Physical surface of the road. May either be specified as a single global value for the segment, or as an array of surface rules.",
-    ),
-    UniqueItemsConstraint(),
-]
+Surfaces = NewType(
+    "Surfaces",
+    Annotated[
+        list[SurfaceRule],
+        Field(
+            min_length=1,
+            description="Physical surface of the road. May either be specified as a single global value for the segment, or as an array of surface rules.",
+        ),
+        UniqueItemsConstraint(),
+    ],
+)
 
-WidthRules = Annotated[
-    list[WidthRule],
-    Field(
-        min_length=1,
-        description="""Edge-to-edge width of the road modeled by this segment, in meters.
+WidthRules = NewType(
+    "WidthRules",
+    Annotated[
+        list[WidthRule],
+        Field(
+            min_length=1,
+            description="""Edge-to-edge width of the road modeled by this segment, in meters.
 
         Examples: (1) If this segment models a carriageway without sidewalk, this value represents the edge-to-edge width of the carriageway, inclusive of any shoulder. (2) If this segment models a sidewalk by itself, this value represents the edge-to-edge width of the sidewalk. (3) If this segment models a combined sidewalk and carriageway, this value represents the edge-to-edge width inclusive of sidewalk.""",
-    ),
-    UniqueItemsConstraint(),
-]
+        ),
+        UniqueItemsConstraint(),
+    ],
+)
