5a46a8a35bdb28991c44931eb306cd33ab896b6b
Seth Fitzsimmons <sethfitz@amazon.com>
2025-08-07 10:44:15 -0700

    Replace type aliases with NewType to improve type checks
    This allows string aliases (for example) to be treated differently;
    i.e., a CountryCode and a RegionCode are both strings, but they're
    fundamentally incompatible because they represent different things. mypy
    will now enforce this at type checking time (when used in a Python
    context).

--- packages/overture-schema-base-theme/src/overture/schema/base/types.py
+++ packages/overture-schema-base-theme/src/overture/schema/base/types.py
@@ -1,28 +1,40 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, NewType
 
 from pydantic import Field
 
-Elevation = Annotated[
-    int,
-    Field(
-        le=9000,
-        description="Elevation above sea level (in meters) of the feature.",
-    ),
-]
+Elevation = NewType(
+    "Elevation",
+    Annotated[
+        int,
+        Field(
+            le=9000,
+            description="Elevation above sea level (in meters) of the feature.",
+        ),
+    ],
+)
 
-Depth = Annotated[
-    int,
-    Field(
-        ge=0,
-        description="Depth below surface level (in meters) of the feature.",
-    ),
-]
+Depth = NewType(
+    "Depth",
+    Annotated[
+        int,
+        Field(
+            ge=0,
+            description="Depth below surface level (in meters) of the feature.",
+        ),
+    ],
+)
 
-Height = Annotated[float, Field(gt=0, description="Height of the feature in meters.")]
+Height = NewType(
+    "Height",
+    Annotated[float, Field(gt=0, description="Height of the feature in meters.")],
+)
 
-SourceTags = Annotated[
-    dict[str, Any],
-    Field(
-        description="Any attributes/tags from the original source data that should be passed through."
-    ),
-]
+SourceTags = NewType(
+    "SourceTags",
+    Annotated[
+        dict[str, Any],
+        Field(
+            description="Any attributes/tags from the original source data that should be passed through."
+        ),
+    ],
+)
