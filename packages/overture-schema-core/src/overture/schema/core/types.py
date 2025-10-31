91c786d854b0ad7ac20a13ca00f2fcff4bcf52ad
schapper <schapper@amazon.com>
2025-10-31 16:25:08 -0700

    Migrate cartography into is own module
    This commit also fixes one definite bug, and one weird thing that was
    arguably a bug.
    
    Definite bug: `Prominence` was modeled as `lt=100`, which would
    indicate a peculiar-sized 99-element range from 1 to 99 inclusive, but
    the actual data in the latest Overture release, `v2025_10_22_0`, ranges
    from 1 to 100 inclusive.
    
    Weird thing: Sort key had a default of zero, and zero is also supposed
    to be the highest rendering priority. This seems weird. It would mean
    that you can't use the sort key to give any feature higher rendering
    priority than a feature that has no rendering priority set. This won't
    work as expected, so I removed the default.
    
    To fix another day: prominence and sort key are reversed. A high number
    means high prominence, but a high number for sort key^[ means a low
    rendering priority.

--- packages/overture-schema-core/src/overture/schema/core/types.py
+++ packages/overture-schema-core/src/overture/schema/core/types.py
@@ -27,61 +27,6 @@ FeatureVersion = NewType(
     "FeatureVersion", Annotated[int32, Field(ge=0, description="")]
 )
 
-Prominence = NewType(
-    "Prominence",
-    Annotated[
-        int,
-        Field(
-            ge=1,
-            lt=100,
-            description="Represents Overture's view of a place's significance or importance.  This value can be used to help drive cartographic display of a place and is derived from various factors including, but not limited to: population, capital status, place tags, and type.",
-        ),
-    ],
-)
-
-MinZoom = NewType(
-    "MinZoom",
-    Annotated[
-        int,
-        Field(
-            ge=0,
-            le=23,
-            description="""Recommended minimum tile zoom per the Slippy Maps convention.
-
-The Slippy Maps zooms are explained in the following references:
-- https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-- https://www.maptiler.com/google-maps-coordinates-tile-bounds-projection""",
-        ),
-    ],
-)
-
-MaxZoom = NewType(
-    "MaxZoom",
-    Annotated[
-        int,
-        Field(
-            ge=0,
-            le=23,
-            description="""Recommended maximum tile zoom per the Slippy Maps convention.
-
-The Slippy Maps zooms are explained in the following references:
-- https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-- https://www.maptiler.com/google-maps-coordinates-tile-bounds-projection""",
-        ),
-    ],
-)
-
-SortKey = NewType(
-    "SortKey",
-    Annotated[
-        int,
-        Field(
-            # default=0,
-            description="An ascending numeric that defines the recommended order features should be drawn in. Features with lower number should be shown on top of features with a higher number.",
-        ),
-    ],
-)
-
 # this is an enum in the JSON Schema, but that prevents OvertureFeature from being extended
 Theme = Annotated[
     str, Field(description="Top-level Overture theme this feature belongs to")
@@ -94,10 +39,6 @@ __all__ = [
     "ConfidenceScore",
     "FeatureVersion",
     "Level",
-    "MaxZoom",
-    "MinZoom",
-    "Prominence",
-    "SortKey",
     "Theme",
     "Type",
 ]
