10afa0dd6f789900a6691342c0f51269b39a477e
schapper <schapper@amazon.com>
2026-02-11 17:04:01 -0800

    wip - bumpit

--- packages/overture-schema-annex/src/overture/schema/__about__.py
+++ packages/overture-schema-annex/src/overture/schema/__about__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1.dev0"
 
 # TEMPORARY COMMENT updated, again, and again, and again, and again, and again, and again, and again, and again, and again, and again
 # ANOTHER LINE HERE.
