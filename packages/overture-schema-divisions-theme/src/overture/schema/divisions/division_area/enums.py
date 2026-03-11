ac45fe79cc75586e7c0432c8362775f8ff50ea5d
schapper <schapper@amazon.com>
2026-03-11 12:39:25 -0700

    Organize divisions pkg, add missing relationships, tidy and add docs
    This commit organizes the divisions Pydantic package to align with the
    other packages that have been done so far: addresses, base, buildings,
    and places. After this commit there remains only: transportation and
    core (the latter will soon be renamed common).
    
    Organization work done:
    
    1. Common code into `_common.py`.
    2. Each feature type gets its own main module, e.g., `division.py`.
    3. Types used exclusively by a feature type into that feature type's main
       module, regardless of whether the type is an enum, model, NewType, etc.
    4. Eliminate sub-directories.
    5. Curate `__all__` in `__init__.py`.
    
    Missing relationships added:
    
    1. `CapitalOfItem`
    2. `HierarchyItem`
    3. `parent_division_id` in `Division`
    4. `capital_division_ids` in `Division`
    
    Getting this to work required extending the list of relationships in the
    system package's `ref.py`. (On this, note, that Tristan will likely
    shrink it back to managable size when he rationalizes the
    relationships.) It also required solving an issue with Python forward
    references being in the Pydantic field annotations for `CapitalOfItem`
    and `HierarchyItem`, which was breaking codegen. I fixed this issue by
    using `.model_rebuild()` on both models, see `divisions.py`.
    
    Docs tidied and updated:
    
    1. Eliminated all `make docformat` errors pertaining to divisions.
    2. Converted several enums to `DocumentedEnum` so they can have a richer
       presence in the schema reference.
    3. Added missing docstrings and `Field(description="...")` annotations.
    4. Edited and refined some of the documentation wording where it wasn't
       fully up to standard.
    5. Wrapped long doc/desc lines using `textwrap.dedent("...").strip()` so
       the docs are easier to read and edit in a code editor.
    
    Signed-off-by: schapper <schapper@amazon.com>

--- packages/overture-schema-divisions-theme/src/overture/schema/divisions/division_area/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class AreaClass(str, Enum):
-    """Area and boundary class designations."""
-
-    LAND = "land"  # The area does not extend beyond the coastline.
-    MARITIME = "maritime"  # The area extends beyond the coastline, in most cases to the extent of the division's territorial sea, if it has one.
